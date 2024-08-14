16740d93d8d0a56cb693a7407298312620774085
Santher <erickhernandop@gmail.com>
2024-08-14 09:21:27 -0500

    Tablas y relaciones de base de datos

---
+++ lavendida.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-08-2024 a las 16:18:32
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+DROP DATABASE lavendida;
+CREATE DATABASE lavendida;
+USE lavendida
+--
+-- Base de datos: `lavendida`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `clientes`
+--
+
+CREATE TABLE `clientes` (
+  `idCliente` int(11) NOT NULL,
+  `nombreCliente` varchar(64) NOT NULL,
+  `apellidoCliente` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_pedidos`
+--
+
+CREATE TABLE `detalle_pedidos` (
+  `idDetalle` int(11) NOT NULL,
+  `idArticulo` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precioVentaUnitario` float NOT NULL,
+  `idPedido` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos_clientes`
+--
+
+CREATE TABLE `pedidos_clientes` (
+  `idPedido` int(11) NOT NULL,
+  `idCliente` int(11) NOT NULL,
+  `fecha` date NOT NULL,
+  `idTipoVenta` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `idProducto` int(11) NOT NULL,
+  `nombreProducto` int(11) NOT NULL,
+  `existencias` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_venta`
+--
+
+CREATE TABLE `tipo_venta` (
+  `idTipo` int(11) NOT NULL,
+  `nombreTipo` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  ADD PRIMARY KEY (`idCliente`);
+
+--
+-- Indices de la tabla `detalle_pedidos`
+--
+ALTER TABLE `detalle_pedidos`
+  ADD PRIMARY KEY (`idDetalle`),
+  ADD KEY `idArticulo` (`idArticulo`),
+  ADD KEY `idPedido` (`idPedido`);
+
+--
+-- Indices de la tabla `pedidos_clientes`
+--
+ALTER TABLE `pedidos_clientes`
+  ADD PRIMARY KEY (`idPedido`),
+  ADD KEY `idCliente` (`idCliente`),
+  ADD KEY `idTipoVenta` (`idTipoVenta`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`idProducto`);
+
+--
+-- Indices de la tabla `tipo_venta`
+--
+ALTER TABLE `tipo_venta`
+  ADD PRIMARY KEY (`idTipo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `clientes`
+--
+ALTER TABLE `clientes`
+  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_pedidos`
+--
+ALTER TABLE `detalle_pedidos`
+  MODIFY `idDetalle` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `pedidos_clientes`
+--
+ALTER TABLE `pedidos_clientes`
+  MODIFY `idPedido` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `idProducto` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_venta`
+--
+ALTER TABLE `tipo_venta`
+  MODIFY `idTipo` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detalle_pedidos`
+--
+ALTER TABLE `detalle_pedidos`
+  ADD CONSTRAINT `detalle_pedidos_ibfk_1` FOREIGN KEY (`idArticulo`) REFERENCES `productos` (`idProducto`),
+  ADD CONSTRAINT `detalle_pedidos_ibfk_2` FOREIGN KEY (`idPedido`) REFERENCES `pedidos_clientes` (`idPedido`);
+
+--
+-- Filtros para la tabla `pedidos_clientes`
+--
+ALTER TABLE `pedidos_clientes`
+  ADD CONSTRAINT `pedidos_clientes_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `clientes` (`idCliente`),
+  ADD CONSTRAINT `pedidos_clientes_ibfk_2` FOREIGN KEY (`idTipoVenta`) REFERENCES `tipo_venta` (`idTipo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
